4d712c3f4ffe6f395e0386068a30c3000d9ad6a7
victorjambo <victorjambo@live.com>
2021-12-08 02:39:13 +0300

    query single token

--- src/graphql/hooks.ts
+++ src/graphql/hooks.ts
@@ -3,13 +3,20 @@ import { QueryResult, useQuery } from "@apollo/client";
 import { QUERY_TOKENS } from "./queries";
 
 export const useQueryTokens = (
+  owner?: string,
   first: number = 100,
   skip: number = 0
 ): QueryResult => {
+  let variables = {
+    first,
+    skip,
+  };
+  if (owner) {
+    variables["where"] = {
+      owner,
+    };
+  }
   return useQuery(QUERY_TOKENS, {
-    variables: {
-      first,
-      skip,
-    },
+    variables,
   });
 };
